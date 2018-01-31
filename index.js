e5b39725451f327705fdbe6ee6e534a0cff0a7fa
Ulrich Schwickerath <ulrich.schwickerath@cern.ch>
2018-01-31 14:26:52 +0100

    add support for ES 6.1.1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-module.exports = function(kibana) {
-	return new kibana.Plugin({
-		uiExports: {
-			visTypes: ['plugins/relational_filter/relational_filter']
-		}
-	});
-
+export default function(kibana) {
+    return new kibana.Plugin({
+	uiExports: {
+	    visTypes: ['plugins/relational_filter/relational_filter']
+	}
+    });
 }
-
